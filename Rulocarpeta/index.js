3937f0f9c35d45fb1a9bc45154f315e11d8f021c
Luca Luriaud <lucaluriaud@gmail.com>
2023-08-15 16:48:18 -0300

    PwB JS

---
+++ Rulocarpeta/index.js
@@ -0,0 +1,43 @@
+//Actividad 1 Respuesta: [10,2,30,4,5]
+//const numbers = [1, 2, 3, 4, 5];
+//numbers[0] = 10;
+//numbers[2] = 30;
+//console.log(numbers);
+
+//Actividad 2
+//const array = ["nombre", "hamburguesa", "papas"];
+
+//function procesarPedido(pedidos) {
+// const nombre = pedidos.shift();
+// pedidos.unshift("bebida");
+// pedidos.push(nombre);
+// console.log(pedidos);
+// return pedidos
+//}
+
+// console.log(procesarPedido(array));
+
+//Actividad 3
+//let numero = [1, 2, 3, 4, 5, 6, 7];
+
+//function sumarPares(numeros) {
+//  let suma = 0;
+//  for (let i = 0; i < numeros.length; i++) {
+//    if (numeros[i] % 2 === 0) {
+//      suma = suma + numeros[i];
+//    }
+//  }
+//  console.log(suma); // imprime el elemento en la posición i
+//}
+
+//sumarPares(numero);
+
+//Actividad 4
+let palabras = ["computadora", "casa", "tenga", "marica"];
+
+function terminanEnA(palabras) {
+  const terminaEnA = palabras.every((palabra) => palabra.endsWith("a"));
+  console.log(terminaEnA);
+}
+
+terminanEnA(palabras);
