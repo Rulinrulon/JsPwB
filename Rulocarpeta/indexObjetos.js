2a20a03ca0b0e90f3dfdcb4e17e8cef35cd00b5f
Rulinrulon <lucaluriaud@gmail.com>
2023-08-22 16:54:27 -0300

    Objetos enteros

--- Rulocarpeta/indexObjetos.js
+++ Rulocarpeta/indexObjetos.js
@@ -1,15 +1,21 @@
-function createObject(name, subs) {
-  const persona = {
-    nombre: name,
-    subscriptores: subs,
-    hash: name.length * subs,
-    getStatus: function () {
-      const mensaje = `El canal de ${name} tiene ${subs} subscriptores`;
-      return mensaje;
-    },
-  };
-  return persona;
-}
+//Actividad 1 Objetos
+//function createObject(name, subs) {
+//  const persona = {
+//    nombre: name,
+//    subscriptores: subs,
+//    hash: name.length * subs,
+//    getStatus: function () {
+//      const mensaje = `El canal de ${name} tiene ${subs} subscriptores`;
+//      return mensaje;
+//    },
+//  };
+//  return persona;
+//}
+//
+//let persona = createObject("Luca", 100);
+//console.log(persona.getStatus());
 
-let persona = createObject("Luca", 100);
-console.log(persona.getStatus());
+//Actividad 2 Objetos
+//Ejercicio 1: Borra el contenido de persona y posteriormente imprime el peso y la altura.
+//Ejercicio 2: Tira error porque esta mal escrito y se pisa entre si.
+//Ejercicio 3: Se llama "Destructuracion".
