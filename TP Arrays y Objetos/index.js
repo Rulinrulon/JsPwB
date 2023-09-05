2ad258899857ededbc949fe028e9851de4177641
Rulinrulon <lucaluriaud@gmail.com>
2023-09-05 14:49:33 -0300

    pagina final AYO

--- TP Arrays y Objetos/index.js
+++ TP Arrays y Objetos/index.js
@@ -4,7 +4,7 @@ const items = [
     img: (src =
       "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg"),
     title: "Avengers: Endgame",
-    year: "2019",
+    year: 2019,
   },
 
   {
@@ -12,7 +12,7 @@ const items = [
     img: (src =
       "https://www.tematika.com/media/catalog/Ilhsa/Imagenes/622990.jpg"),
     title: "El Conjuro 2",
-    year: "2016",
+    year: 2016,
   },
 
   {
@@ -20,14 +20,14 @@ const items = [
     img: (src =
       "https://es.web.img2.acsta.net/pictures/14/04/30/11/55/592219.jpg"),
     title: "Harry Potter",
-    year: "2001",
+    year: 2001,
   },
 
   {
     id: 4,
     img: (src = "https://pics.filmaffinity.com/AaREC-273731015-large.jpg"),
     title: "REC",
-    year: "2007",
+    year: 2007,
   },
 
   {
@@ -35,7 +35,7 @@ const items = [
     img: (src =
       "https://m.media-amazon.com/images/M/MV5BMDJhMGRjN2QtNDUxYy00NGM3LThjNGQtMmZiZTRhNjM4YzUxL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"),
     title: "Shrek 2",
-    year: "2004",
+    year: 2004,
   },
 
   {
@@ -43,7 +43,7 @@ const items = [
     img: (src =
       "https://m.media-amazon.com/images/M/MV5BNzlkNzVjMDMtOTdhZC00MGE1LTkxODctMzFmMjkwZmMxZjFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"),
     title: "Rapidos y Furiosos",
-    year: "2001",
+    year: 2001,
   },
 ];
 
@@ -56,16 +56,16 @@ function genereteItemsHTML() {
     const button = document.createElement("button");
     const h2 = document.createElement("h2");
     const img = document.createElement("img");
-    const h3 = document.createElement("h3");
+    const year = document.createElement("year");
 
     h2.textContent = item.title;
     img.src = item.img;
     img.alt = item.title;
-    h3.textContent = item.description;
+    year.textContent = item.year;
 
     button.appendChild(h2);
     button.appendChild(img);
-    button.appendChild(h3);
+    button.appendChild(year);
 
     li.appendChild(button);
     itemsContainer.appendChild(li);
